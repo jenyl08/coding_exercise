469ca8c927c4c4fc23490db868bd374d2d3688b5
jenyl08 <jenyl08@gmail.com>
2020-04-15 22:26:32 +0900

    리트코드 챌린지 30 첫 주 업데이트

--- LeetCode/solution.py
+++ LeetCode/solution.py
@@ -116,6 +116,22 @@ class Solution(object):
             return_list.append(dic_anas[list_str])
 
         return return_list
+    def countElements(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: int
+        """
+
+        return_count = 0
+        set_arr = set(arr)
+
+        for i in arr:
+
+            j = int(i + 1)
+            if j in set_arr:
+                return_count = return_count + 1
+
+        return return_count
 
 s=Solution()
 
@@ -138,3 +154,5 @@ print(s.maxProfit(input_data))
 input_data = ["eat", "tea", "tan", "ate", "nat", "bat"] #groupAnagrams
 print(s.groupAnagrams(input_data))
 
+input_data = [1,3,2,3,5,0] #countElements
+print(s.countElements(input_data))
