af23afecfbc13c57839ef4d86c7de701108504ef
jenyl08 <jenyl08@gmail.com>
2020-04-15 22:50:29 +0900

    리트코드 챌린지 30 첫 주 업데이트

--- LeetCode/Happy Number/isHappy.py
+++ LeetCode/Happy Number/isHappy.py
@@ -25,7 +25,42 @@ class Solution(object):
                     return False
                 else:
                     set_nums.add(n)
+
+    def isHappy2(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+
+        self.isAlready = set([])
+
+        def hn(n):
+            if n == 0:
+                return 0
+            elif n == 1:
+                return 1
+            else:
+                sum = 0
+                while n >= 10:
+                    s = n % 10
+                    sum = sum + s * s
+                    n = int(n / 10)
+                sum = sum + n * n
+                if sum in self.isAlready:
+                    return 0
+                else:
+                    self.isAlready.add(sum)
+                    return hn(sum)
+
+        if hn(n) == 1:
+            return True
+        else:
+            return False
+
 s=Solution()
 
 input_data = 19 #isHappy
 print(s.isHappy(input_data))
+
+input_data = 19 #isHappy2
+print(s.isHappy2(input_data))
--- LeetCode/solution.py
+++ LeetCode/solution.py
@@ -41,6 +41,36 @@ class Solution(object):
                     return False
                 else:
                     set_nums.add(n)
+    def isHappy2(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+
+        self.isAlready = set([])
+
+        def hn(n):
+            if n == 0:
+                return 0
+            elif n == 1:
+                return 1
+            else:
+                sum = 0
+                while n >= 10:
+                    s = n % 10
+                    sum = sum + s * s
+                    n = int(n / 10)
+                sum = sum + n * n
+                if sum in self.isAlready:
+                    return 0
+                else:
+                    self.isAlready.add(sum)
+                    return hn(sum)
+
+        if hn(n) == 1:
+            return True
+        else:
+            return False
     def maxSubArray(self, nums):
         """
         :type nums: List[int]
@@ -140,6 +170,9 @@ print(s.singleNumber(input_data))
 
 input_data = 19 #isHappy
 print(s.isHappy(input_data))
+
+input_data = 19 #isHappy2
+print(s.isHappy2(input_data))
 
 input_data = [-2,1,-3,4,-1,2,1,-5,4] #maxSubArray
 print(s.maxSubArray(input_data))
